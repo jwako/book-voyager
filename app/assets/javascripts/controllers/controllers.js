f7d3aeb9a6d476b1c3cb85b1c9b59538029d7204
Junya Wako <j.wako@j-q.co.jp>
2014-03-14 11:24:50 +0900

    Fix controllers

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -3,10 +3,11 @@ var bookControllers = angular.module('bookControllers', ['infinite-scroll']);
 bookControllers.controller('BookCtrl', function($scope, $http, Book) {
 	$scope.page = 1;
 	$scope.items = [];
+  $scope.category = "";
 
   $scope.loadMore = function() {
   	$scope.busy = true;
-    Book.getBooks($scope.page).then(function(book) {
+    Book.getBooks($scope.page, $scope.category).then(function(book) {
       $scope.busy = false;
       for (var i = 0; i < book.data.length; i++) {
         $scope.items.push(book.data[i]);
@@ -22,10 +23,23 @@ bookControllers.controller('CategoryCtrl', function($scope, $http) {
     $scope.tree = data;
   });
 
-  $scope.getChildCategories = function(category) {
+  $scope.refresh = function(category) {
+    getChildCategories(category);
+    getBooks(category.node_id);
+  }
+
+  function getChildCategories(category) {
     $http.get('/v1/categories?bn=' + category.node_id).success(function(data) {
       category.nodes = data;
     });
+  }
+
+  function getBooks(category) {
+    var scope = angular.element('#books').scope();
+    scope.page = 1 ;
+    scope.category = category;
+    scope.items = [];
+    scope.loadMore();
   }
 
 });
--- app/assets/javascripts/services/services.js
+++ app/assets/javascripts/services/services.js
@@ -2,8 +2,8 @@ var bookServices = angular.module('bookServices', []);
 
 bookServices.factory('Book', function($http) {
 	var bookService = {
-		getBooks : function(page) {
-			var response = $http.get('/v1/books/' + page + '.json').success(function(data) {
+		getBooks : function(page, category) {
+			var response = $http.get('/v1/books/' + page + '.json?bn=' + category).success(function(data) {
 				return data;
 			});
 			return response;
