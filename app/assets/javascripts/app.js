179bcb7831d6c27344c6b5c1d70fa1f19564b674
Junya Wako <j.wako@j-q.co.jp>
2014-03-22 08:34:51 +0900

    Fix for turbolinks

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,3 +2,11 @@ var bookvoyagerApp = angular.module('bookvoyagerApp', [
 	'bookControllers',
 	'bookServices'
 ]);
+
+// For turbolinks
+$(document).on('page:load', function() {
+  $('[ng-app]').each(function () {
+		module = $(this).attr('ng-app');
+    angular.bootstrap(this, [module]);  	
+  });
+});
